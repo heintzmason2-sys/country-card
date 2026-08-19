3a7d7b8c1249a1fe7cef9198fa2a5cb8700408fc
kingMason0116 <heintzmason2@gmail.com>
2026-08-19 13:55:58 -0700

    Initial Country Card project

---
+++ app.js
@@ -0,0 +1,63 @@
+const countryContainer = document.getElementById("country-container");
+const bttn = document.getElementById("search-button");
+const clearButton = document.getElementById("clear-button");
+const inputValue = document.getElementById("search-country");
+
+
+bttn.addEventListener("click", async function(){
+    try{
+        const response = await fetch(`https://countries.dev/name/${inputValue.value}`)
+        if(!response.ok){
+            throw new Error("Country Not Found!")
+        }
+        const data = await response.json();
+
+        
+        
+        for(let i = 0; i < data.length && i < 3; i++){
+            const card = document.createElement("div")
+            card.classList.add("card")
+
+            const name = document.createElement("h1");
+            name.textContent = `Country Name: ${data[i].name}`;
+
+            const capital = document.createElement("p")
+            capital.textContent = `Capital: ${data[i].capital || "Not Available"}`
+
+    
+
+            const currency = document.createElement("p")
+            currency.textContent = `Currency: ${data[i].currencies[0].name}`
+
+            const currencySymbol = document.createElement("p")
+            currencySymbol.textContent = `Currency Symbol: ${data[i].currencies[0].symbol}`
+
+            const population = document.createElement("p")
+            population.textContent = `Population: ${data[i].population}`
+
+            const flag = document.createElement("img")
+            flag.src = data[i].flags.png;
+
+            
+            
+            card.appendChild(flag);
+            card.appendChild(name);
+            card.appendChild(capital);
+            card.appendChild(currency);
+            card.appendChild(currencySymbol);
+            card.appendChild(population);
+
+            countryContainer.appendChild(card)
+
+        };
+
+      
+
+    }catch(error){
+       console.log(error)
+    }
+});
+
+clearButton.addEventListener("click", function(){
+    countryContainer.innerHTML = "";
+})
