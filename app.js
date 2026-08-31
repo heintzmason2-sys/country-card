34431393375a2b6ab5d89fcdca2904f90594b02a
kingMason0116 <heintzmason2@gmail.com>
2026-08-30 19:27:09 -0700

    Polish Country Card project

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const countryContainer = document.getElementById("country-container");
 const bttn = document.getElementById("search-button");
 const clearButton = document.getElementById("clear-button");
 const inputValue = document.getElementById("search-country");
+const countryModal = document.getElementById("country-modal");
+const closeCountryModal = document.getElementById("close-country-modal");
+const modalFlag = document.getElementById("modal-flag");
+const modalCountryName = document.getElementById("modal-country-name");
+const modalRegion = document.getElementById("modal-region");
+const modalSubregion = document.getElementById("modal-subregion");
+const modalNativeName = document.getElementById("modal-native-name");
+const modalDemonym = document.getElementById("modal-demonym");
+const modalArea = document.getElementById("modal-area");
+const modalDensity = document.getElementById("modal-density");
+const modalTimezone = document.getElementById("modal-timezone");
+const modalLanguage = document.getElementById("modal-language");
+
+closeCountryModal.addEventListener("click", function(){
+    countryModal.style.display = "none";
+})
 
 
 bttn.addEventListener("click", async function(){
@@ -18,6 +34,19 @@ bttn.addEventListener("click", async function(){
         for(let i = 0; i < data.length && i < 3; i++){
             const card = document.createElement("div")
             card.classList.add("card")
+            card.addEventListener("click", function(){
+                modalCountryName.textContent = data[i].name;
+                modalFlag.src = data[i].flags.png;
+                modalRegion.textContent = `Region: ${data[i].region  || "Not Available"}`;
+                modalSubregion.textContent = `Subregion: ${data[i].subregion || "Not Available"}`;
+                modalNativeName.textContent = `Native Name: ${data[i].nativeName || "Not Available"}`;
+                modalDemonym.textContent  = `Demonym: ${data[i].demonym || "Not Available"}`;
+                modalArea.textContent = `Area: ${data[i].area || "Not Available"} km²`;
+                modalDensity.textContent = `Population Density: ${data[i].populationDensity || "Not Available"} people/km²`;
+                modalTimezone.textContent = `Timezone: ${data[i].timezones?.join(", ") || "Not Available"}`;
+                modalLanguage.textContent = `Languages: ${data[i].languages.map(language => language.name).join(",")}`;
+                countryModal.style.display = "block";
+            })
 
             const name = document.createElement("h1");
             name.textContent = `Country Name: ${data[i].name}`;
@@ -51,12 +80,16 @@ bttn.addEventListener("click", async function(){
             card.appendChild(population);
 
             countryContainer.appendChild(card)
+            console.log(data[i])
 
         };
 
       
 
     }catch(error){
+       const errorMessage = document.createElement("p")
+       errorMessage.textContent = "Country not found. Please try another search."
+       countryContainer.appendChild(errorMessage)
        console.log(error)
     }
 });
